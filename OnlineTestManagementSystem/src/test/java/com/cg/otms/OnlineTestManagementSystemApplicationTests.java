f0234312a2a06d389fe6cdf11962f7b2452e01fe
'girija' <'gitijas'>
2020-09-23 19:05:10 +0530

    updated

--- OnlineTestManagementSystem/src/test/java/com/cg/otms/OnlineTestManagementSystemApplicationTests.java
+++ OnlineTestManagementSystem/src/test/java/com/cg/otms/OnlineTestManagementSystemApplicationTests.java
@@ -1,9 +1,13 @@
 package com.cg.otms;
 
+import java.math.BigInteger;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.cg.otms.dto.User;
 import com.cg.otms.service.TestService;
 
 @SpringBootTest
@@ -11,11 +15,40 @@ class OnlineTestManagementSystemApplicationTests {
 	@Autowired
 	TestService testservice;
 	@Test
-	public void testdeletetest_positive() throws Exception
-	
+	public void testAssignTest_Positive()
 	{
-		//BigInteger Id=111;
-		//String s =testservice.deleteTest(Id);
+		BigInteger b1;
+		 b1 = new BigInteger("111");
+		User user = testservice.assignTest(b1,"student1");
+		Assertions.assertNotNull(user);
+	}
+	
+	@Test
+	public void testAssignTest_Negative()
+	{
+		BigInteger b2;
+		 b2 = new BigInteger("111");
+		User user = testservice.assignTest(b2,"student10");
+		Assertions.assertNull(user);
+	}
+	
+	
+	@Test
+	public void testDeleteTest_Positive()
+	{
+		BigInteger b3;
+		 b3 = new BigInteger("111");
+		String s = testservice.deleteTest(b3);
+		Assertions.assertEquals("deleted successfully",s);
 	}
 
+	@Test
+	public void testDeleteTest_Negative()
+	{
+		BigInteger b4;
+		 b4 = new BigInteger("250");
+		String s = testservice.deleteTest(b4);
+		Assertions.assertNull(s);
+	}
+	
 }
